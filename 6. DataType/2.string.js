fac5adc7c5607c3c52ad14e8c77fb10201d84db8
wooniwinnie <wooniwinnie@gmail.com>
2023-06-11 13:24:51 +0900

    string type

---
+++ 6. DataType/2.string.js
@@ -0,0 +1,12 @@
+var string;
+string = '문자열';
+string = '문자열';
+string = `문자열`;
+
+var first = 'Jiwoon';
+var last = 'Jeon';
+
+console.log(`My name is ` + first + ' ' + last + '.');
+console.log(`My name is ${first} ${last}.`);
+console.log(`1+ 2 = ${1 + 2}`);
+console.log('1 + 2 = ${1 + 2}');
